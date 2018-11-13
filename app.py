3d0f1e5ee2dd74549ca780061e7cbef6e8ac6f2f
Flaminia <flaminic@thoughtworks.com>
2018-11-13 20:37:03 -0300

    get books from db

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from  MySQLdb import connect, cursors
 
 all_my_books = {
     'book1': {
@@ -10,38 +11,47 @@ all_my_books = {
         'title': 'Moby Dick',
         'description': 'This is book about a whale',
         'img_url': 'https://cdn.shopify.com/s/files/1/0036/2812/products/Moby_blue.jpg?v=1527114780'
+    },
+    'book3': {
+        'title': 'Lord of the Rings',
+        'description': 'The saga',
+        'img_url': 'https://images-na.ssl-images-amazon.com/images/I/51EstVXM1UL._SX331_BO1,204,203,200_.jpg'
     }
-
+    ,
+    'book4': {
+        'title': 'The Hobbit',
+        'description': 'The little one',
+        'img_url': 'hhttps://images-na.ssl-images-amazon.com/images/I/51uLvJlKpNL._SX321_BO1,204,203,200_.jpg'
+    }
 }
+
 
 all_my_users = {
     'user1': {
         'first_name': 'John',
         'last_name': 'Smith',
-        'borrowed': None,
-        'given': None,
+        'currentBook': 'book2',
+        'given': ['book3', 'book1'],
     },
     'user2': {
         'first_name': 'Thomas',
         'last_name': 'Adams',
-        'borrowed': None,
-        'given': None,
+        'currentBook': 'book1',
+        'given': ['book2', 'book4'],
     }
 }
 
-
 app = Flask(__name__)
 
-      # Templates
-@ app.route("/")
 
-
+# Templates
+@app.route("/")
 def home():
     return render_template('index.html')
 
 
 @app.route("/book/<book_id>")
-def book(book_id, book_author):
+def book(book_id):
     book = all_my_books[book_id]
 
     return render_template('book.html',
@@ -50,21 +60,30 @@ def book(book_id, book_author):
                            book_url=book['img_url'])
 
 
+@app.route("/books")
+def books():
+    connection = connect(host='localhost', user='root', passwd='',
+            db='booklix', cursorclass=cursors.DictCursor)
+    cursor = connection.cursor()
+    cursor.execute("SELECT id, title, description, imageUrl FROM books")
+    books = cursor.fetchall()
+    connection.close()
+    return render_template('books.html', books=books)
+
+
 # Templates
 @app.route("/status")
 def status():
     return 'HAPPY'
 
 
-
 @app.route("/user/<user_ID>")
 def user(user_ID):
-    #return user_ID
-
     user = all_my_users[user_ID]
+    borrowedBookId = user['currentBook']
+    borrowedBook = all_my_books[borrowedBookId]
 
-
-    return render_template('user.html', user=user)
+    return render_template('user.html', user=user, borrowedBook=borrowedBook)
 
 
 if __name__ == "__main__":
